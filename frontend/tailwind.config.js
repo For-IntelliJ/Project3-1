7709540be46d30b3666444f96418d2b39cb5d8a0
God-Da <150757592+God-Da@users.noreply.github.com>
2025-03-31 15:08:27 +0900

    테일윈즈 드디어 성공

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // JSX도 포함!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
