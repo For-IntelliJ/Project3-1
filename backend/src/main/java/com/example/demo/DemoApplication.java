1e323ee7fe70cb04e925cac5056ebcfda5902f50
God-Da <150757592+God-Da@users.noreply.github.com>
2025-04-03 17:59:04 +0900

    백엔드, DB 연동 완료

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(UserRepository userRepository) {
+		return args -> {
+			User user = new User("kang", "kang@email.com");
+			userRepository.save(user);
+			System.out.println("✅ User 저장 완료!");
+		};
+	}
 }
---
+++ backend/src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,46 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    // 기본 생성자
+    public User() {}
+
+    // 생성자
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    // Getter & Setter
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
