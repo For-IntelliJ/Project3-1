8f80eda0d2a209a9be7aed242684630b6cb2d3b8
Hyun010612 <wogus0612a@gmail.com>
2025-05-12 17:03:07 +0900

    백,프론트 동시 실행
    과정본

--- backend/src/main/java/com/example/demo/config/CorsConfig.java
+++ backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -1,15 +1,15 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration // 이 클래스가 Spring의 설정 클래스임을 나타냄
-public class CorsConfig implements WebMvcConfigurer { // WebMvcConfigurer를 구현하여 CORS 설정 추가
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000") // 특정 Origin(프론트엔드 주소) 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드 지정
-    }
-}
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration // 이 클래스가 Spring의 설정 클래스임을 나타냄
+//public class CorsConfig implements WebMvcConfigurer { // WebMvcConfigurer를 구현하여 CORS 설정 추가
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
+//                .allowedOrigins("http://localhost:3000") // 특정 Origin(프론트엔드 주소) 허용
+//                .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드 지정
+//    }
+//}
