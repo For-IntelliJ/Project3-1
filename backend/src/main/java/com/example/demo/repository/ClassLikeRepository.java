a66eb1731975e3bcf0883a70a91c450d5e1f4c17
God-Da <150757592+God-Da@users.noreply.github.com>
2025-05-26 15:38:33 +0900

    백엔드 다시 받았어요

--- backend/src/main/java/com/example/demo/repository/ClassLikeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.ClassLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassLikeRepository extends JpaRepository<ClassLike, Long> {
-    // 필요 시 classId, userId 기반 중복 체크 등 추가 가능
-}
