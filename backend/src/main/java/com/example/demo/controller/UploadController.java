f8d13e199937ab1b296bc8f713b55afd200d9e6e
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-06-06 23:36:42 +0900

    EditProfile에서 받은 파일을 서비스로 전송
    제곧내

--- backend/src/main/java/com/example/demo/controller/UploadController.java
+++ backend/src/main/java/com/example/demo/controller/UploadController.java
@@ -4,22 +4,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.example.demo.service.S3Service;
-
-
 import java.util.Map;
 
+//프론트에서 formData로 보낸 사진 파일을 받는 부분
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/profile")
+@CrossOrigin(origins = "http://localhost:3001", allowCredentials = "true")
+
 public class UploadController {
 
     private final S3Service s3Service;
 
-    public UploadController(S3Service s3Service) {
+    public UploadController(S3Service s3Service) { //생성자 생성
         this.s3Service = s3Service;
     }
 
     @PostMapping("/upload")
     public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
+        System.out.println("uploadImage 호출됨, 파일 이름: " + file.getOriginalFilename());
         try {
             String imageUrl = s3Service.uploadFile(file);
             return ResponseEntity.ok().body(Map.of("imageUrl", imageUrl));
@@ -27,4 +30,5 @@ public class UploadController {
             return ResponseEntity.status(500).body("업로드 실패: " + e.getMessage());
         }
     }
+
 }
